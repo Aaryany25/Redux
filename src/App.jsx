fe8730f057bab894b67c2f8b445b3ffb930f9dc6
Aaryan <aaryany2505@gmail.com>
2026-02-19 23:00:52 +0530

    added Input and can increment number by input

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { decrement, increment, incrementbyAmount } from './redux/features/counterSlice'
 
 function App() {
+  const [num, setnum] = useState(5)
   const dispatch = useDispatch()
   const count = useSelector((state)=>state.counter.value)
   return (
@@ -15,8 +16,11 @@ dispatch(increment())
 dispatch(decrement())
 
       }}>-</button>
+      <input type="number"  value={num} onChange={(e)=>{
+        setnum(e.target.value)
+      }} />
       <button onClick={()=>{
-dispatch(incrementbyAmount(5))
+dispatch(incrementbyAmount(Number(num)))
 
       }}>Increment by Amount</button>
     </div>
